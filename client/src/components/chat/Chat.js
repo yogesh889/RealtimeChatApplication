19fe47b225ce1ace414024ecf06ea49e1d52bfb3
Yogesh889 <8058yogesh@gmail.com>
2023-10-07 18:42:49 +0530

    Update Chat.js

--- client/src/components/chat/Chat.js
+++ client/src/components/chat/Chat.js
@@ -16,7 +16,7 @@ const Chat = () => {
   const [users, setUsers] = useState('');
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  const ENDPOINT = "localhost:5000"; // Replace with your actual server URL
+  const ENDPOINT = "https://realtimechatapplicationproject.000webhostapp.com/"; // Replace with your actual server URL
 
   const location = useLocation(); // Use useLocation hook to get the location object
 
